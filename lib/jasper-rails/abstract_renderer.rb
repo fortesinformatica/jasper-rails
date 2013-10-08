225a801a5a1a97d801f9b2dc494783d0879ae79d
Gregório Kusowski <gregorio.kusowski@gmail.com>
2013-10-08 09:23:06 -0300

    Avoiding empty if statement.

--- lib/jasper-rails/abstract_renderer.rb
+++ lib/jasper-rails/abstract_renderer.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 module JasperRails
-    
+
   class AbstractRenderer
 
     class_attribute :after_fill_blocks
@@ -8,26 +8,25 @@ module JasperRails
 
     def self.register(file_extension, options = {}, &block)
       renderer = self.new(file_extension, options, &block)
-  
+
       if Mime::Type.lookup_by_extension(file_extension).nil?
         Mime::Type.register options[:mime_type], file_extension.to_sym
       end
-      
+
       ActionController::Responder.send(:define_method, "to_#{file_extension}") do
-        if template_name = self.options[:template]
-        else
-          template_name = "#{controller.controller_path}/#{controller.action_name}"
-        end
+        template_name = self.options[:template]
+        template_name ||= "#{controller.controller_path}/#{controller.action_name}"
+
         details_options = {:formats => [:jrxml], :handlers => []}
 
         jrxml_file = controller.lookup_context.find_template(template_name,[],false,[],details_options).identifier
-        jasper_file = jrxml_file.sub(/\.jrxml$/, ".jasper")      
-  
+        jasper_file = jrxml_file.sub(/\.jrxml$/, ".jasper")
+
         params = {}
         controller.instance_variables.each do |v|
           params[v.to_s[1..-1]] = controller.instance_variable_get(v)
         end
-  
+
         response_options = JasperRails.config[:response_options].merge(:type => options[:mime_type])
         controller.send_data renderer.render(jasper_file, resource, params, self.options), response_options
       end
